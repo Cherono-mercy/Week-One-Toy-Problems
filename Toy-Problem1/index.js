c4aa7877b0a9b5c5516cd7549f9b37c125c5fed0
Mercy Cherono <cheronom142@gmail.com>
2023-09-25 12:51:04 +0800

    Final solution to my toy problem 1

--- Toy-Problem1/index.js
+++ Toy-Problem1/index.js
@@ -9,16 +9,22 @@ function studentGradeGenerator() {
 
   //Using the if..else statement to grade the student's marks and printing out the grade in bold
   if (studentMarks > 79 && studentMarks <= 100) {
-    document.write("The student's grade is: " + "<b>" + "A" + "<b>");
+    document.getElementById("demo").innerHTML =
+      "The student's grade is:" + "<b>" + "A" + "<b>";
   } else if (studentMarks <= 79 && studentMarks >= 60) {
-    document.write("The students grade is: " + "<b>" + "B" + "<b>");
+    document.getElementById("demo").innerHTML =
+      "The student's grade is:" + "<b>" + "B" + "<b>";
   } else if (studentMarks <= 59 && studentMarks > 49) {
-    document.write("The stdent's grade is: " + "<b>" + "C" + "<b>");
+    document.getElementById("demo").innerHTML =
+      "The student's grade is:" + "<b>" + "C" + "<b>";
   } else if (studentMarks <= 49 && studentMarks >= 40) {
-    document.write("The student's grade is: " + "<b>" + "D" + "<b>");
+    document.getElementById("demo").innerHTML =
+      "The student's grade is:" + "<b>" + "D" + "<b>";
   } else if (studentMarks < 40 && studentMarks >= 0) {
-    document.write("The student's grade is: " + "<b>" + "E" + "<b>");
+    document.getElementById("demo").innerHTML =
+      "The student's grade is:" + "<b>" + "E" + "<b>";
   } else if (studentMarks < 0 || studentMarks > 100) {
-    document.write("Please input a value between 0 and 100");
+    document.getElementById("demo").innerHTML =
+      "Please input a value between 0 and 100";
   }
 }
